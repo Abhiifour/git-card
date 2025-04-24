7e802d9071765080274d340aa496ee5ae36b0daf
Abhishek Singh <as8998013@gmail.com>
2025-04-25 02:44:09 +0530

    v-1

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ import Card from "@/components/Card";
 import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { GoGitMerge } from "react-icons/go";
+import { FaGitAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { IoGitPullRequestOutline } from "react-icons/io5";
+import { SiGithubactions } from "react-icons/si";
+import { SiGithubcopilot } from "react-icons/si";
+import { SiGithubsponsors } from "react-icons/si";
 
 
 
@@ -11,7 +18,7 @@ export default function Home() {
   const [user , setUser] = useState("")
   const [userData , setUserData] = useState<any>()
 
-  const handleSubmit = async ()=>{
+   const handleSubmit = async ()=>{
     const res = await axios.get(`http://127.0.0.1:8787/user/${user}`)
     console.log(res.data)
     setUserData(res.data.data)
@@ -19,22 +26,47 @@ export default function Home() {
 
   
   return (
-    <div className="flex justify-center items-center h-screen font-inter">
-{/*      
+    <div className="absolute flex justify-center items-center h-screen w-full font-inter">
+       <GoGitMerge className="absolute text-emerald-400 opacity-10 text-9xl top-10 left-10" />
+      <FaGitAlt className="absolute text-emerald-400 opacity-10 text-9xl top-20 right-20" />
+      <FaGithub className="absolute text-emerald-400 opacity-10 text-9xl bottom-20 left-20" />
+      <IoGitPullRequestOutline className="absolute text-emerald-400 opacity-10 text-9xl bottom-10 right-10" />
+      <SiGithubactions className="absolute text-emerald-400 opacity-10 text-9xl top-1/2 left-1/4 transform -translate-y-1/2" />
+      <SiGithubcopilot className="absolute text-emerald-400 opacity-10 text-9xl top-1/4 right-1/4 transform -translate-x-1/2" />
+      <SiGithubsponsors className="absolute text-emerald-400 opacity-10 text-9xl bottom-1/4 left-1/3 transform -translate-x-1/2" />
+     
       {
         userData ?
-        <Card userData={userData}/> : 
-        <div className="flex flex-col justify-center items-center gap-4 border rounded-4xl p-9"> 
-        <h1>
+        <div className="absolute top-0 left-0 right-0 flex items-center justify-center h-screen ">
+        {/* Concentric circles */}
+        <div className="absolute w-[400px] h-[480px] rounded-3xl border border-emerald-400/30"></div>
+        <div className="absolute w-[390px] h-[460px] rounded-3xl border border-emerald-400/40"></div>
+        <div className="absolute w-[380px] h-[440px] rounded-3xl border border-emerald-400/50"></div>
+        <div className="absolute w-[370px] h-[420px] rounded-3xl border border-emerald-400/60"></div>
+        
+        {/* App icon */}
+        <div className="relative z-10">
+        <Card userData={userData}/>
+        </div>
+
+      <div className="absolute top-20 left-16 text-emerald-400 text-2xl">✦</div>
+      <div className="absolute top-16 right-16 text-emerald-400 text-2xl">✦</div>
+      <div className="absolute bottom-24 left-24 text-emerald-400 text-2xl">✦</div>
+      <div className="absolute bottom-24 right-32 text-emerald-400 text-2xl">✦</div>
+  </div>
+: 
+        <div className="relative z-10 flex flex-col justify-center items-center gap-4 border rounded-4xl p-9 "> 
+        <h1 className="font-semibold text-lg">
           Please add you github username to continue
         </h1>
         <input type="text" value={user} onChange={(e) => setUser(e.target.value)} className="border py-1 px-2"/>
 
         <button onClick={handleSubmit} className="border p-1 rounded-lg px-2 cursor-pointer">Continue</button>
         </div>
-      } */}
+      }
 
-      <Card/>
+
+      
 
     </div>
   );
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,19 +1,35 @@
 "use client"
+
 import Image from "next/image";
-export default function Card(){
+export default function Card({userData}:{userData:any}){
+    const date = new Date(userData.created_at);
     return (
-        <div className="border rounded-4xl w-[400px] h-[400px] p-6">
-          <div className="bg-gray-500 w-[50px] h-[50px] rounded-full">
-            {/* <Image
-            src="https://avatars.githubusercontent.com/u/116870561?v=4"
+        <div className="shadow-3xl rounded-4xl w-[350px] p-8 font-inter bg-gradient-to-b from-[#2C2C2C] to-[#111111]  text-white tracking-tight">
+          <p className="text-[10px] mb-4 flex justify-start items-center gap-1"> <span className="w-2 h-2 bg-green-600 rounded-full mb-[1px]"></span>{userData.location}</p>
+          <div className="flex justify-start items-center gap-4 ">
+          <div className=" w-[60px] h-[60px] rounded-full overflow-hidden">
+            <img
+            src={userData.avatar}
             alt="Placeholder Image"
-            width={50}
-            height={50}
-            /> */}
+           
+            />
           </div>
-          <p>Abhishek</p>
-          <p>Bio sdfljsdlfjlsdjlfjldfjds</p>
-          <div>Blog</div>
+          <div>
+          <p className="text-base text-[#F1F1F1]">{userData.name}</p>
+          <p className="text-sm text-[#B0B0B0]">@{userData.username}</p>
+          </div>
+          </div>
+          <p className="text-base mt-4">{userData.bio}</p>
+
+          
+          <div className="tracking-tight  grid grid-cols-2 gap-2 mt-4">
+          <p className="text-sm py-4 px-3  rounded-lg text-[#B0B0B0] border bg-[#2f2f2f] border-[#474646]  flex flex-col justify-center items-center text-center"><a href="https://abhiifour.xyz">{userData.followers} <br></br>followers</a></p>
+          <p className="text-sm py-4 px-3 rounded-lg text-[#B0B0B0] border bg-[#2f2f2f] border-[#474646]  flex flex-col justify-center items-center text-center"><a href="https://abhiifour.xyz">{userData.following} <br></br> following</a></p>
+          <p className="text-sm py-4 px-3 rounded-lg text-[#B0B0B0] border bg-[#2f2f2f] border-[#474646] flex flex-col justify-center items-center text-center"><a href="https://abhiifour.xyz">{userData.gist}  <br></br>gists</a></p>
+          <p className="text-sm py-4 px-3 rounded-lg text-[#B0B0B0] border bg-[#2f2f2f] border-[#474646]  flex flex-col justify-center items-center text-center"><a href="https://abhiifour.xyz">{userData.repos} <br></br> repos</a></p>
+          
+          </div>
+          <p className="text-xs tracking-tight text-[#B0B0B0] mt-4">Joined on {date.toDateString()}</p>
         </div>
     )
 }
